59bfbb174c26abb30f95eaf00045912f8eae820b
neysanataniiia <79122123+neysanataniiia@users.noreply.github.com>
2022-04-04 16:22:40 +0700

    Add files via upload

---
+++ BiroJodoh/src/main/java/com/rplbo/Perempuan.java
@@ -0,0 +1,21 @@
+package com.rplbo;
+
+public class Perempuan {
+    public String nama;
+    public int usia;
+    public String status;
+    public Double jumlahCinta;
+    public Pria jodoh;
+    public Pria temanKencan;
+    public Double standarJumlahCinta;
+
+    public Perempuan(String nama, int usia, double v) {
+        this.nama = nama;
+        this.usia = usia;
+    }
+    private void MengajakKencan(Pria pria){
+
+    }
+
+
+}
---
+++ BiroJodoh/src/main/java/com/rplbo/Pria.java
@@ -0,0 +1,24 @@
+package com.rplbo;
+
+public class Pria {
+    private String nama;
+    private int usia;
+    private String status;
+    private Double jumlahCinta;
+    private Perempuan jodoh;
+    private Perempuan temanKencan;
+
+    public Pria(String nama, int usia) {
+        this.nama = nama;
+        this.usia = usia;
+    }
+
+
+    public void PDKT(Perempuan calon){}
+
+    public void MengajakKencan(Perempuan perempuan){}
+
+    public void MenembakKekasih(){}
+
+
+}
